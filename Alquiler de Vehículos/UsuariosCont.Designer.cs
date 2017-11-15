287f4afd3d4e526b0952ebd54d80bee6158f1c1d
oscarvillacorta <ozkar.villacorta0915@outlook.com>
2017-11-15 16:39:03 -0600

    Se agregó un TabControl al formulario de usuarios

--- Alquiler de Vehículos/UsuariosCont.Designer.cs
+++ Alquiler de Vehículos/UsuariosCont.Designer.cs
@@ -28,11 +28,85 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Usuarios";
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(540, 321);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtcodigo);
+            this.tabPage1.Controls.Add(this.lblcodigo);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(532, 295);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Administración";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Búsqueda";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(32, 27);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblcodigo.TabIndex = 0;
+            this.lblcodigo.Text = "Codigo";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(183, 27);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 1;
+            // 
+            // UsuariosCont
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 321);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "UsuariosCont";
+            this.Text = "Usuarios";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Label lblcodigo;
+    }
 }
--- Alquiler de Vehículos/UsuariosCont.cs
+++ Alquiler de Vehículos/UsuariosCont.cs
@@ -16,5 +16,10 @@ namespace Alquiler_de_Vehículos
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
